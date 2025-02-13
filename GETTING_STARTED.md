bcc3395614ad59a6cd9f13d623a53d45d3025699
Brian Curtis <64433609+BrianCurtis-NOAA@users.noreply.github.com>
2025-02-13 15:33:03 -0500

    Add scripts to allow workflow to run (only for WCOSS2 for now) (#44)
    Bring in multiple scripts to assist users in getting a very basic workflow using eclfow started and completed.
    ---------
    
    Co-authored-by: Anna Smoot <anna.smoot@clogin09.cactus.wcoss2.ncep.noaa.gov>

---
+++ GETTING_STARTED.md
@@ -0,0 +1,8 @@
+STEPS TO GET WORKFLOW RUNNING (FOR NOW)
+ * edit user_settings.sh
+ * bash setup_workflow.sh
+ * ssh into ecflow node (Using -X for ecflow_ui) 
+ * cd into AQM-Workflow
+ * source setup_ecflow.sh
+ * edit top primary section of ecf/defs/aqm_testing.def (edit to match your package structure for now)
+ * bash run_workflow.sh
---
+++ setup_ecflow.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+#
+# This is the setup script for AQM Workflow.
+# It currently only applies to WCOSS2.
+# Please develop this script knowing it will eventually apply to multiple systems.
+
+# Bring in user settings
+source user_settings.sh
+
+# ECFlow Server Settings
+export ECF_HOST=${ECF_HOST:-}
+export ECF_PORT=${ECF_PORT:-}
+
+# ECFlow Settings
+export ECF_DATA_ROOT=${ecflow_dir}
+export ECF_OUTPUTDIR=${ecflow_dir}/output
+export ECF_COMDIR=${ecflow_dir}/submit
+export LFS_OUTPUTDIR=${ECF_COMDIR}
+export ECF_LISTS=${ECF_HOST}.${ECF_PORT}.ecf.lists
+export ECF_INCLUDE=${workflow_dir}/ecf/include
